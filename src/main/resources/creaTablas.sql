6177df3a4447bb65e532bcabc27e6393afcdeb1e
mariramirez29 <mariana.ingcomputacion@gmail.com>
2026-08-10 14:24:29 -0600

    Semana 5b. CRUD Producto Completo con BD en AIVEN.IO

--- src/main/resources/creaTablas.sql
+++ src/main/resources/creaTablas.sql
@@ -1,29 +1,12 @@
+-- Sección de administración (ejecutar una vez en un entorno de desarrollo)
+-- Sección de administración (ejecutar una vez en un entorno de desarrollo)
 /*
   Script de creación de base de datos para TechShop
-  Este script crea el esquema, tablas, usuarios, y
-  carga datos de ejemplo.
+  Este script crea las tablas y carga datos de ejemplo.
 */
--- Sección de administración (ejecutar una vez en un entorno de desarrollo)
-drop database if exists techshop;
-drop user if exists usuario_prueba;
-drop user if exists usuario_reportes;
 
--- Creación del esquema
-CREATE database techshop
-  DEFAULT CHARACTER SET utf8mb4
-  DEFAULT COLLATE utf8mb4_unicode_ci;
-
--- Creación de usuarios con contraseñas seguras (idealmente asignadas fuera del script)
-create user 'usuario_prueba'@'%' identified by 'Usuar1o_Clave.';
-create user 'usuario_reportes'@'%' identified by 'Usuar1o_Reportes.';
-
--- Asignación de permisos
--- Se otorgan permisos específicos en lugar de todos los permisos a todas las tablas futuras
-grant select, insert, update, delete on techshop.* to 'usuario_prueba'@'%';
-grant select on techshop.* to 'usuario_reportes'@'%';
-flush privileges;
-
-use techshop;
+-- Base de datos remota en Aiven
+USE defaultdb;
 
 -- --- Sección de Creación de Tablas ---
 
