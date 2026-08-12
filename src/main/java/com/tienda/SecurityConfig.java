6ebfebf24209ba5891cadfbea71918ba26de7ffa
mariramirez29 <mariana.ingcomputacion@gmail.com>
2026-08-11 19:13:57 -0600

    Semana 10. Seguridad con BD

--- src/main/java/com/tienda/SecurityConfig.java
+++ src/main/java/com/tienda/SecurityConfig.java
@@ -1,60 +1,58 @@
 package com.tienda;
 
+import com.tienda.domain.Ruta;
+import com.tienda.service.RutaService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    public static final String[] PUBLIC_URL = {
-        "/",
-        "/index",
-        "/fav/**",
-        "/js/**",
-        "/webjars/**",
-        "/login"
-    };
-
-    public static final String[] USUARIO_URL = {
-        "/facturar/carrito"
-    };
-
-    public static final String[] ADMIN_OR_VENDEDOR_URL = {
-        "/producto/listado",
-        "/categoria/listado"
-    };
-
-    public static final String[] ADMIN_URL = {
-        "/producto/**",
-        "/categoria/**",
-        "/usuario/**",
-        "/consultas/**"
-    };
-
+    // Este método genera el proceso de autorización
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            @Lazy RutaService rutaService)
             throws Exception {
 
-        http.authorizeHttpRequests(request -> request
-                .requestMatchers(PUBLIC_URL).permitAll()
-                .requestMatchers(USUARIO_URL).hasRole("USUARIO")
-                .requestMatchers(ADMIN_OR_VENDEDOR_URL)
-                    .hasAnyRole("ADMIN", "VENDEDOR")
-                .requestMatchers(ADMIN_URL).hasRole("ADMIN")
-                .anyRequest().authenticated()
-        ).formLogin(form -> form
+        var rutas = rutaService.getRutas();
+
+        http.authorizeHttpRequests(requests -> {
+
+            for (Ruta ruta : rutas) {
+
+                if (ruta.isRequiereRol()) {
+
+                    requests
+                            .requestMatchers(ruta.getRuta())
+                            .hasRole(ruta.getRol().getRol());
+
+                } else {
+
+                    requests
+                            .requestMatchers(ruta.getRuta())
+                            .permitAll();
+                }
+            }
+
+            requests.anyRequest().authenticated();
+
+        });
+
+        http.formLogin(form -> form
                 // Configuración de formulario de login
                 .loginPage("/login")
                 .loginProcessingUrl("/login")
@@ -85,28 +83,18 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // Este método será reemplazado la siguiente semana
-    @Bean
-    public UserDetailsService users(PasswordEncoder passwordEncoder) {
+    /*
+     * Se configura Spring Security para utilizar el
+     * UserDetailsService que obtiene los usuarios desde la BD.
+     */
+    @Autowired
+    public void configurarGlobal(
+            AuthenticationManagerBuilder build,
+            @Lazy PasswordEncoder passwordEncoder,
+            @Lazy UserDetailsService userDetailsService)
+            throws Exception {
 
-        UserDetails juan = User.builder()
-                .username("juan")
-                .password(passwordEncoder.encode("123"))
-                .roles("ADMIN")
-                .build();
-
-        UserDetails rebeca = User.builder()
-                .username("rebeca")
-                .password(passwordEncoder.encode("456"))
-                .roles("VENDEDOR")
-                .build();
-
-        UserDetails pedro = User.builder()
-                .username("pedro")
-                .password(passwordEncoder.encode("789"))
-                .roles("USUARIO")
-                .build();
-
-        return new InMemoryUserDetailsManager(juan, rebeca, pedro);
+        build.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
     }
 }
